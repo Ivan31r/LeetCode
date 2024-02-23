1ce990be2ecd179290e20eaaf0f4a1d29c51e389
IUsov <ivan-vanes007@rambler.ru>
2024-02-23 12:29:59 +0300

    26. Remove Duplicates from Sorted Array

---
+++ src/main/algorithms/removeDuplicatesFromSortedArray/Solution.java
@@ -0,0 +1,23 @@
+package main.algorithms.removeDuplicatesFromSortedArray;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+//        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+//        int[] nums = {1, 1, 2};
+        int[] nums = {1, 2};
+        System.out.println("unique counter numbers = " + removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int uniqueCounterNumber = 1;
+        for (int currentIndex = 1; currentIndex < nums.length; currentIndex++) {
+            if (nums[currentIndex] != nums[uniqueCounterNumber - 1]) {
+                nums[uniqueCounterNumber++] = nums[currentIndex];
+            }
+        }
+        return uniqueCounterNumber;
+    }
+}
